4ce626232b35905d44ff67d78dba72c8ff83098a
natalietthomas <121183224+natalietthomas@users.noreply.github.com>
2026-09-15 16:48:29 -0400

    Set up Flask templates and routes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return app.send_static_file('index.html')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
